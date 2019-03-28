d4dae032c2c4bbb88895fb593ce48a60a0a7b174
Eman Ali <emanehab99@gmail.com>
2019-03-29 07:23:53 +1100

    Add a home page

--- galaxiaui/settings/base.py
+++ galaxiaui/settings/base.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'galaxiaweb',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'galaxiaui.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, '../templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,6 +120,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, '../static-files/')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "../static/"),
+    os.path.join(BASE_DIR, "../galaxiaweb/static/"),
+]
 
 LOGGING = {
     'version': 1,
