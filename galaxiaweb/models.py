c4df312c00bbdd15842a621d6e281a6f980dcdee
Eman Ali <emanehab99@gmail.com>
2019-04-09 16:43:47 +1000

    changed the model to add magnitude name values (S4 List)

--- galaxiaweb/models.py
+++ galaxiaweb/models.py
@@ -51,10 +51,63 @@ class JobParameter(models.Model):
 
     PLACEHOLDER = [(PLACEHOLDER1,PLACEHOLDER1)]
 
+    MAGNITUDE_NAME_CHOICES = [
+        (GAIA_G, GAIA_G),
+        (GAIA_GBP, GAIA_GBP),
+        (GAIA_GRP, GAIA_GRP),
+        (TMASS_J, TMASS_J),
+        (TMASS_H, TMASS_H),
+        (TMASS_KS, TMASS_KS),
+        (WISE_W1, WISE_W1),
+        (WISE_W2, WISE_W2),
+        (WISE_W3, WISE_W3),
+        (WISE_W4, WISE_W4),
+        (IRAC_36, IRAC_36),
+        (IRAC_45, IRAC_45),
+        (IRAC_58, IRAC_58),
+        (IRAC_80, IRAC_80),
+        (MIPS_24, MIPS_24),
+        (MIPS_70, MIPS_70),
+        (MIPS_160, MIPS_160),
+        (TESS_TE, TESS_TE),
+        (KEPLER_KE, KEPLER_KE),
+        (SDSS_G, SDSS_G),
+        (SDSS_R, SDSS_R),
+        (SDSS_I, SDSS_I),
+        (SDSS_Z, SDSS_Z),
+        (KEPLER_DDO51, KEPLER_DDO51),
+        (PANS1_G, PANS1_G),
+        (PANS1_R, PANS1_R),
+        (PANS1_I, PANS1_I),
+        (PANS1_Z, PANS1_Z),
+        (PANS1_Y, PANS1_Y),
+        (PANS1_W, PANS1_Y),
+        (UBV_U, UBV_U),
+        (UBV_B, UBV_B),
+        (UBV_V, UBV_V),
+        (UBV_R, UBV_R),
+        (UBV_I, UBV_I),
+        (UBV_J, UBV_J),
+        (UBV_H, UBV_H),
+        (UBV_K, UBV_K),
+        (STROEMGREN_V1, STROEMGREN_V1),
+        (STROEMGREN_U, STROEMGREN_U),
+        (STROEMGREN_V, STROEMGREN_V),
+        (STROEMGREN_B, STROEMGREN_B),
+        (STROEMGREN_Y, STROEMGREN_Y),
+        (STROEMGREN_HB_W, STROEMGREN_HB_W),
+        (STROEMGREN_HB_N, STROEMGREN_HB_N),
+        (DENIS_I, DENIS_I),
+        (DENIS_J, DENIS_J),
+        (DENIS_KS, DENIS_KS),
+        (TYCHO_BT, TYCHO_BT),
+        (TYCHO_VT, TYCHO_VT),
+    ]
+
     model_file = models.CharField(choices=MODEL_FILE_CHOICES, max_length=55, blank=False, null=False, default=SHARMA_2019)
 
-    magnitude_name = models.CharField(choices=PLACEHOLDER, max_length=55, blank=False, null=False,
-                                        default=PLACEHOLDER1)
+    magnitude_name = models.CharField(choices=MAGNITUDE_NAME_CHOICES, max_length=55, blank=False, null=False,
+                                        default=GAIA_G)
     apparent_magnitude_min = models.FloatField(blank=False, null=False, default=-100.0)
     apparent_magnitude_max = models.FloatField(blank=False, null=False, default=14.0)
 
@@ -70,10 +123,10 @@ class JobParameter(models.Model):
     absolute_magnitude_min = models.FloatField(blank=False, null=False, default=-100.0)
     absolute_magnitude_max = models.FloatField(blank=False, null=False, default=100.0)
 
-    magnitude_name_1 = models.CharField(choices=PLACEHOLDER, max_length=55, blank=False, null=False,
-                                  default=PLACEHOLDER1)
-    magnitude_name_2 = models.CharField(choices=PLACEHOLDER, max_length=55, blank=False, null=False,
-                                        default=PLACEHOLDER1)
+    magnitude_name_1 = models.CharField(choices=MAGNITUDE_NAME_CHOICES, max_length=55, blank=False, null=False,
+                                  default=GAIA_G)
+    magnitude_name_2 = models.CharField(choices=MAGNITUDE_NAME_CHOICES, max_length=55, blank=False, null=False,
+                                        default=GAIA_GBP)
 
     geometry_options = models.CharField(choices=GEOMETRY_OPTIONS, max_length=55, blank=False, null=False,
                                         default=ALL_SKY)
--- galaxiaweb/static/galaxiaweb/js/job_parameter_form.js
+++ galaxiaweb/static/galaxiaweb/js/job_parameter_form.js
@@ -1,3 +1,20 @@
+
 $(document).ready(function() {
 
+    // photo_sys_2 hide/show options on change.
+    check_photo_sys_2_type();
+    $('body').on('change', '#id_photo_sys_2',
+        function() {
+            check_photo_sys_2_type();
+        });
+
+    function check_photo_sys_2_type() {
+        if ($('#id_photo_sys_2').find(":selected").text() == 'GAIADR2_TMASS')
+        {
+
+        }
+        else{
+
+        };
+    }
 });
