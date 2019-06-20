ddf1d17efffa91fb7f5370b068f9ddc4777726c8
Eman Ali <emanehab99@gmail.com>
2019-06-20 15:43:53 +1000

    Fix job_detail URL not correctly displayed

--- galaxiaweb/views/job.py
+++ galaxiaweb/views/job.py
@@ -2,6 +2,8 @@
 Distributed under the MIT License. See LICENSE.txt for more info.
 """
 
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.shortcuts import render, redirect, get_object_or_404
 from ..forms.job_parameter import JobParameterForm
 from ..models import Job, JobParameter
@@ -20,26 +22,29 @@ def new_job(request):
             job = Job.objects.create()
             form.instance.job = job
             form.save(commit=True)
-            return render(
-                request,
-                "galaxiaweb/job/job_detail.html",
-                {'job': form.instance}
-            )
+            job_key = form.instance.job_key
+            return HttpResponseRedirect(reverse('job_detail', args=(job_key,)))
+            # return render(
+            #     request,
+            #     "galaxiaweb/job/job_detail.html",
+            #     {'job_key': job_key}
+            # )
         else:
             return render(
                 request,
                 "galaxiaweb/job/new_job.html",
                 {'job_parameter_form': form}
             )
-    else:
-        form = JobParameterForm()
-        return render(
-            request,
-            "galaxiaweb/job/new_job.html",
-            {'job_parameter_form': form}
-        )
+
+    form = JobParameterForm()
+    return render(
+        request,
+        "galaxiaweb/job/new_job.html",
+        {'job_parameter_form': form}
+    )
 
 
-def job_detail(request, key):
-    job = get_object_or_404(JobParameter, job_key=key)
+def job_detail(request, job_key):
+    print(job_key)
+    job = get_object_or_404(JobParameter, job_key=job_key)
     return render(request, 'galaxiaweb/job/job_detail.html', {'job': job})
