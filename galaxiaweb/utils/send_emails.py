363932267db9b6273c4f4d3e83043de1a785f659
Eman Ali <emanehab99@gmail.com>
2019-08-23 12:38:16 +1000

    remove the full stop following parameter file link in email templates

--- galaxiaweb/utils/send_emails.py
+++ galaxiaweb/utils/send_emails.py
@@ -9,14 +9,14 @@ def send_email(to, job_key, link, output_link): #needs url
     from django.template import Template,Context
 
     html_template = Template('Dear Galaxia User,\n Your job {{ job_id }} is complete. \n'
-                             ' You can download the parameter file here: {{ job_link }}. \n'
+                             ' You can download the parameter file here: {{ job_link }} \n'
                              ' And the output file by following this link: {{ output_link }}')
     html_context = Context({'job_id': job_key,'job_link': link, 'output_link': output_link})
 
     html_final_content = html_template.render(html_context)
 
     txt_template = ('Dear Galaxia User,\n Your job {{ job_id }} is complete. \n'
-                    ' You can download the parameter file here: {{ job_link }}. \n'
+                    ' You can download the parameter file here: {{ job_link }} \n'
                     ' And the output file by following this link: {{ output_link }}')
 
     msg = EmailMultiAlternatives(subject='Your Galaxia Job status', from_email="noreply@swin.edu.au",to=to, body=html_final_content)
